61d0a0e3a229b565f805881dc9b06a027dac11a0
wiccs <wiccs1236@gmail.com>
2025-05-28 19:06:44 -0600

    Intento 18 de despliegue :(

--- src/main/java/com/example/SACHI/controller/HomeController.java
+++ src/main/java/com/example/SACHI/controller/HomeController.java
@@ -1,17 +1,13 @@
 package com.example.SACHI.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class HomeController {
 
-@GetMapping("/")  // Mapeamos la ruta raíz
-    public String index() {
-        return "index";  // Thymeleaf buscará la plantilla 'index.html'
-    }
-
-    @GetMapping("/registro")
-    public String registrar() {
-        return "RegisterUser";
+    @GetMapping("/")
+    public String home() {
+        return "SACHI está funcionando!";
     }
 }
